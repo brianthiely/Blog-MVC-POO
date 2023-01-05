d4560df456728fa00fff3f0eab671535109a4d03
Brian <brian.thiely@gmail.com>
2023-01-05 19:06:15 +0100

    fix invalid return type

--- src/Core/Db.php
+++ src/Core/Db.php
@@ -35,7 +35,7 @@ class Db extends PDO
      *
      * @return static
      */
-    public static function getInstance(): Db
+    public static function getInstance(): self
     {
         if (self::$instance === null) {
             self::$instance = new self();
